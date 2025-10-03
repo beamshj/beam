862c63b9d2d83dd26e0a589059f05517501e27f6
rahees <raheez1995india@gmail.com>
2025-10-03 14:24:16 +0530

    social media link

--- app/components/layout/NavBar.tsx
+++ app/components/layout/NavBar.tsx
@@ -192,21 +192,21 @@ const NavBar = () => {
 
               {/* Icons */}
               <div className="flex gap-[7px] p-6">
-                <div className="rounded-full xl:w-[46px] xl:h-[46px] w-[36px] h-[36px] border border-black/35 hover:border-transparent flex items-center justify-center hover:bg-primary cursor-pointer transition-colors duration-300">
+                <Link href="https://www.facebook.com/beamedusocial/" target="_blank" className="rounded-full xl:w-[46px] xl:h-[46px] w-[36px] h-[36px] border border-black/35 hover:border-transparent flex items-center justify-center hover:bg-primary cursor-pointer transition-colors duration-300">
                   <FaFacebookF className="text-sm" />
-                </div>
-                <div className="rounded-full xl:w-[46px] xl:h-[46px] w-[36px] h-[36px] border border-black/35 hover:border-transparent flex items-center justify-center hover:bg-primary cursor-pointer transition-colors duration-300">
+                </Link>
+                <Link href="https://x.com/beamedusocial" target="_blank" className="rounded-full xl:w-[46px] xl:h-[46px] w-[36px] h-[36px] border border-black/35 hover:border-transparent flex items-center justify-center hover:bg-primary cursor-pointer transition-colors duration-300">
                   <FaXTwitter className="text-sm" />
-                </div>
-                <div className="rounded-full xl:w-[46px] xl:h-[46px] w-[36px] h-[36px] border border-black/35 hover:border-transparent flex items-center justify-center hover:bg-primary cursor-pointer transition-colors duration-300">
+                </Link>
+                <Link href="https://www.linkedin.com/company/bukhatireducation/" target="_blank" className="rounded-full xl:w-[46px] xl:h-[46px] w-[36px] h-[36px] border border-black/35 hover:border-transparent flex items-center justify-center hover:bg-primary cursor-pointer transition-colors duration-300">
                   <FaLinkedinIn className="text-sm" />
-                </div>
-                <div className="rounded-full xl:w-[46px] xl:h-[46px] w-[36px] h-[36px] border border-black/35 hover:border-transparent flex items-center justify-center hover:bg-primary cursor-pointer transition-colors duration-300">
+                </Link>
+                <Link href="https://www.instagram.com/beamedusocial/" target="_blank" className="rounded-full xl:w-[46px] xl:h-[46px] w-[36px] h-[36px] border border-black/35 hover:border-transparent flex items-center justify-center hover:bg-primary cursor-pointer transition-colors duration-300">
                   <FaInstagram className="text-sm" />
-                </div>
-                <div className="rounded-full xl:w-[46px] xl:h-[46px] w-[36px] h-[36px] border border-black/35 hover:border-transparent flex items-center justify-center hover:bg-primary cursor-pointer transition-colors duration-300">
+                </Link>
+                <Link href="https://www.youtube.com/c/BukhatirEducation" target="_blank" className="rounded-full xl:w-[46px] xl:h-[46px] w-[36px] h-[36px] border border-black/35 hover:border-transparent flex items-center justify-center hover:bg-primary cursor-pointer transition-colors duration-300">
                   <FaYoutube className="text-sm" />
-                </div>
+                </Link>
               </div>
             </div>
           </div>
