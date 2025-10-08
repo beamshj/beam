5223221718db26da55d9650df93bf025e6a7242e
Hisham I Globalsurf <hisham@globalsurf.ae>
2025-10-08 12:48:27 +0530

    feat(founderMessage): component created

---
+++ app/components/FounderMessage/data.ts
@@ -0,0 +1,17 @@
+export const BannerData = {
+  BannerTitle: "Founders' Message",
+  BannerImg: "/images/founderMessage/Banner.jpg",
+};
+
+export const founderMessageData = {
+  title: `Founders'
+  Message`,
+  photo: "/images/founderMessage/founder.png",
+  name: "Salah A. Bukhatir",
+  designation: "Founder & Chairman BEAM",
+  description: `It gives me great pleasure to welcome you to Bukhatir Education Advancement and Management (BEAM), a unique environment that sets new standards in the education sector, built on the profound trust posed in us by parents, students, staff and the tremendous success achieved over the last two decades.
+  BEAM is leading our endeavors to replicate the vision behind ISCS and ASCS Sharjah at other campuses under the banner of American School of Creative Science (ASCS) and International School of Creative (ISCS) across the UAE and GCC.
+It is our promise to nurture and develop ambitious, committed young boys and girls, men and women, who upon graduation will have preserved their cultural identities and become confident intellectuals, serving as role models in the modern world today and tomorrow.
+We remain fully committed to striving for excellence and to do our utmost to ensure delivery of the highest quality academics within an environment that fosters traditional values and ethos that define our credo.
+Welcome to Bukhatir Education Advancement and Management (BEAM)`,
+};
