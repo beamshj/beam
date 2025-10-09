0590dfc7c9669a93f406b2fbb3d9fbd2af513932
rahees <raheez1995india@gmail.com>
2025-10-09 16:34:50 +0530

    contact pg resp

--- app/components/ContactUs/sections/OurSchools.tsx
+++ app/components/ContactUs/sections/OurSchools.tsx
@@ -22,7 +22,7 @@ export default function OurSchools({ data }: { data: OurSchoolsProps }) {
     <section className="pb-12 md:pb-20 xl:pb-[135px]">
       <div className="container overflow-hidden">
         {/* Title + Description */}
-        <div className="mb-5 xl:mb-[30px]">
+        <div className="mb-5 md:mb-8 xl:mb-[30px]">
           <h2 className="text-lg lg:text-2xl xl:text-3xl 2xl:text-4xl font-light text-black leading-[1.1111]">
             {title}
           </h2>
@@ -32,7 +32,7 @@ export default function OurSchools({ data }: { data: OurSchoolsProps }) {
         </div>
 
         {/* Cards */}
-        <div className="flex flex-col md:flex-row gap-[19px] justify-between">
+        <div className="flex flex-wrap xl:flex-row gap-[19px] justify-between">
           {schools.map((school, i) => {
             const isActive = activeIndex === i;
 
@@ -49,19 +49,12 @@ export default function OurSchools({ data }: { data: OurSchoolsProps }) {
                   duration: 0.6,
                   ease: "easeInOut",
                 }}
-                className={`relative rounded-[12px] overflow-hidden h-[350px] xl:h-[544px] cursor-pointer flex-shrink-0 schl ${
+                className={`relative rounded-[12px] overflow-hidden h-[300px] md:h-[350px] xl:h-[544px] cursor-pointer flex-shrink-0 schl ${
                   isActive ? "z-20" : "z-10"
                 }`}
               >
                 {/* Image */}
-                <Image
-                  src={school.img}
-                  alt={school.title}
-                  fill
-                  className={`object-cover transition-all duration-500 ${
-                    isActive ? "grayscale-0" : "grayscale"
-                  }`}
-                />
+                <Image src={school.img} alt={school.title} fill className={`object-cover transition-all duration-500 ${ isActive ? "grayscale-0" : "grayscale" }`} />
 
                 {/* Overlay */}
                 {!isActive && (
@@ -90,17 +83,12 @@ export default function OurSchools({ data }: { data: OurSchoolsProps }) {
                 {/* Location Pill */}
                 {isActive && school.location && (
                   <motion.div
-                    className="absolute top-[42px] left-[40px] z-10 bg-[#E6F7FF] font-light text-sm leading-[1.42] px-[20px] py-[11px] rounded-[50px] inline-flex items-center gap-[8px] text-black"
+                    className="absolute top-[42px] left-5 xl:left-[40px] z-10 bg-[#E6F7FF] font-light text-sm leading-[1.42] px-[20px] py-[11px] rounded-[50px] inline-flex items-center gap-[8px] text-black"
                     initial={{ opacity: 0, x: 80 }}
                     animate={{ opacity: 1, x: 0 }}
                     transition={{ duration: 0.8, ease: "easeOut" }}
                   >
-                    <Image
-                      src="/images/contact-us/icons/location.svg"
-                      alt="location"
-                      width={24}
-                      height={24}
-                    />
+                    <Image src="/images/contact-us/icons/location.svg" alt="location" width={24} height={24} />
                     {school.location}
                   </motion.div>
                 )}
@@ -108,51 +96,30 @@ export default function OurSchools({ data }: { data: OurSchoolsProps }) {
                 {/* Active / Inactive Content */}
                 {isActive ? (
                   // ACTIVE CARD (expanded)
-                  <div className="absolute bottom-[40px] left-[40px] right-[40px] flex flex-col md:flex-row justify-between items-end text-white z-10 gap-6 md:gap-0">
+                  <div className="absolute bottom-[40px] left-5 xl:left-[40px] xl:right-[40px] flex flex-col xl:flex-row justify-between xl:items-end text-white z-10 gap-6 xl:gap-0">
                     {/* Left content */}
-                    <motion.div
-                      initial={{ opacity: 0, x: 80 }}
-                      animate={{ opacity: 1, x: 0 }}
-                      transition={{ duration: 0.8, ease: "easeOut" }}
-                      className="flex flex-col"
-                    >
-                      <h3 className="text-xl font-light leading-[1.2]">
+                    <motion.div initial={{ opacity: 0, x: 80 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.8, ease: "easeOut" }} className="flex flex-col" >
+                      <h3 className="text-md lg:text-lg xl:text-xl font-light leading-[1.2]">
                         {school.title}
                       </h3>
                       <div className="mt-[15px] inline-flex items-center text-sm font-light">
                         Learn more{" "}
-                        <Image
-                          src="/images/arrow-right-tip.svg"
-                          alt="arrow"
-                          width={25}
-                          height={24}
-                          className="ml-[12px]"
-                        />
+                        <Image src="/images/arrow-right-tip.svg" alt="arrow" width={25} height={24} className="ml-[12px]" />
                       </div>
                     </motion.div>
 
                     {/* Right arrow button */}
                     <div className="mt-4 md:mt-0">
-                      <span className="w-[74px] h-[74px] flex items-center justify-center border border-white rounded-full">
-                        <Image
-                          src="/images/arrow-primary.svg"
-                          alt="arrow"
-                          width={24}
-                          height={24}
-                        />
+                      <span className="w-12 h-12 xl:w-[74px] xl:h-[74px] flex items-center justify-center border border-white rounded-full">
+                        <Image src="/images/arrow-primary.svg" alt="arrow" width={24} height={24} className="w-auto h-4 xl:h-[24px]" />
                       </span>
                     </div>
                   </div>
                 ) : (
                   // INACTIVE CARD: only arrow centered at bottom
-                  <div className="absolute bottom-[40px] left-1/2 -translate-x-1/2 z-10">
+                  <div className="absolute bottom-[40px] left-5 xl:left-1/2 xl:-translate-x-1/2 z-10">
                     <span className="w-[74px] h-[74px] flex items-center justify-center border border-white rounded-full">
-                      <Image
-                        src="/images/arrow-primary.svg"
-                        alt="arrow"
-                        width={24}
-                        height={24}
-                      />
+                      <Image src="/images/arrow-primary.svg" alt="arrow" width={24} height={24} />
                     </span>
                   </div>
                 )}
