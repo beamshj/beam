28f8007b2142d2e126739d93682432a7aa089809
Hisham I Globalsurf <hisham@globalsurf.ae>
2025-10-09 11:27:07 +0530

    fix(type issues)

--- app/components/ContactUs/data.ts
+++ app/components/ContactUs/data.ts
@@ -2,3 +2,31 @@ export const BannerData = {
   BannerTitle: "Contact us",
   BannerImg: "/images/contact-us/banner.jpg",
 };
+
+export const OurSchoolsData = {
+  title: "Our Schools",
+  description:
+    "Explore our expanding network of schools and stay updated on key developments.",
+  schools: [
+    {
+      title: "ASCS Maliha",
+      location: "Maliha Road, Sharjah",
+      img: "",
+    },
+    {
+      title: "ASCS Al Ittihad",
+      location: "Al Ittihad Road, Sharjah",
+      img: "",
+    },
+    {
+      title: "ASCS Al Ittihad",
+      location: "Al Ittihad Road, Sharjah",
+      img: "",
+    },
+    {
+      title: "ASCS Al Ittihad",
+      location: "Al Ittihad Road, Sharjah",
+      img: "",
+    },
+  ],
+};
