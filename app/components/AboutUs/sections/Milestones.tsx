169eedfab7357d730b5fe571c710add3a3b6b6fd
jishnum007 <jishnu@globalsurf.ae>
2025-10-13 14:15:58 +0530

    about slider

--- app/components/AboutUs/sections/Milestones.tsx
+++ app/components/AboutUs/sections/Milestones.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper/modules";
+import { Navigation, Pagination,Autoplay } from "swiper/modules";
+import { motion } from "framer-motion";
 import "swiper/css";
 import "swiper/css/navigation";
-import "swiper/css/pagination";
+import "swiper/css/pagination"; 
+
 export interface ValueItem {
   year: string;
   title: string;
@@ -17,81 +20,172 @@ interface Props {
   items: ValueItem[];
 }
 
-export default function MoreToExplore({ items }: Props) { 
+
+export default function MoreToExplore({ items }: Props) {
   
+  const [activeIndex, setActiveIndex] = useState(0);
+   
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const [containerMarginLeft, setContainerMarginLeft] = useState(0);
+  useEffect(() => {
+    const updateMargin = () => {
+      if (containerRef.current) {
+        const rect = containerRef.current.getBoundingClientRect();
+        const offsetLeft = rect.left;
+        setContainerMarginLeft(offsetLeft);
+      }
+    };
+
+    updateMargin();
+    window.addEventListener("resize", updateMargin);
+    return () => window.removeEventListener("resize", updateMargin);
+  }, []);
   return (
-    <section className="pt-10 pb-0 xl:py-20 2xl:py-[135px] bg-[#F6F6F6] overflow-hidden">
-      <div className="container relative ">
-        <h2 className="text-lg xl:text-2xl 2xl:text-4xl font-light mb-4 xl:mb-8 2xl:mb-[50px]">
+    <section className="pt-10 xl:pt-20 2xl:pt-[135px] pb-[80px] xl:pb-[100px] 2xl:pb-[165px] bg-[#F6F6F6] overflow-hidden">
+      <div className="container " ref={containerRef}>
+        <h2 className="text-lg xl:text-2xl 2xl:text-4xl font-light md:mb-4 xl:mb-8 2xl:mb-[50px]">
           Building Milestones
         </h2>
-        <div className="mt-[155px]">
-        <div className="relative">
-      <Swiper
-        spaceBetween={0}
-        slidesPerView={1}
-        breakpoints={{
-          768: { slidesPerView: 1},
-          1024: { slidesPerView: 1 },
-        }}
-        navigation={{
-          nextEl: ".swiper-button-next-custom",
-          prevEl: ".swiper-button-prev-custom",
-        }} 
-        pagination={{
-          el: ".swiper-pagination-custom",
-          clickable: true,
-        }}
-        modules={[Navigation, Pagination]}
-        className="!overflow-visible"
-      >
-        {items.map((item, index) => (
-          <SwiperSlide key={index} className="relative">
-            <div className="mb-9 flex flex-col items-start relative">
-              
-              {/* Year Badge */}
-              <div className="mb-5 flex items-center border border-black rounded-full px-[15px] py-[7px] w-fit yrmn ">
-                <p className="text-md md:text-lg 2xl:text-xl font-light text-black leading-[1]">
-                  {item.year}
-                </p>
-              </div>
+        </div>
 
-              {/* Timeline Dot + Line */}
-              <div className="relative mb-5 w-full">
-                <div className="w-[48px] h-[48px] bgseta    rounded-full flex items-center justify-center relative z-[2]">
-                  <div className="w-[24px] h-[24px] bgactive rounded-full "></div>
-                </div>
-                <div className="absolute top-[25px] left-[28px] w-full h-[1px] bg-gradient-to-r from-[#7E5AA3] to-[#23ABD2]"></div>
-              </div>
+        <div className=" relative"style={{
+          paddingInline: "15px",
+              marginLeft: containerMarginLeft ? `${containerMarginLeft}px` : undefined,
+            }} >
+        <div className="mt-5 md:mt-[155px] relative">
+          <Swiper 
+            spaceBetween={0}
+            slidesPerView={1}
+            breakpoints={{
+              0: { slidesPerView: 1 },
+              768: { slidesPerView: 1.1 },
+              1024: { slidesPerView: 1.1 }, 
+              1600: { slidesPerView: 1.05 },
+            }}
+            loop={true}
+            navigation={{
+              nextEl: ".swiper-button-next-custom",
+              prevEl: ".swiper-button-prev-custom",
+            }}
+            pagination={{
+              el: ".swiper-pagination-custom",
+              clickable: true,
+            }}
+            
+            onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
+            modules={[Navigation, Pagination,Autoplay]}
+            autoplay={{ delay: 6000, disableOnInteraction: false }}
+            className="md:!overflow-visible"
+          >
+            {items.map((item, index) => (
+              <SwiperSlide key={index} className="relative">
+                <motion.div className="mb-9 flex flex-col items-start" 
+                  key={activeIndex}
+                  initial={{ opacity: 0, x: -80 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  transition={{ duration: 0.9, ease: "easeOut" }}>
+               
+                  {/* Year Badge (static) */}
+                  <div className="flex items-center border border-black rounded-full px-[15px] py-[7px] w-fit yrmn relative">
+                    <p className="font-light text-black leading-[1]">{item.year}</p>
+                  </div>
 
-              {/* Content */}
-              <div className="mt-5 lg:mt-[71px] vhidden">
-                <p className="text-md md:text-lg 2xl:text-xl font-light text-black leading-[1] mb-3">
-                  {item.title}
-                </p>
-                <p className="text-sm font-light text-colorpara leading-[1.2] max-w-[24ch]">
-                  {item.description}
-                </p>
-              </div>
+                  {/* Timeline Dot + Line (static) */}
+                  <div className="relative w-full dotline hidden md:block">
+                    <div className="w-[48px] h-[48px] bgseta rounded-full flex items-center justify-center relative z-[2]">
+                      <div className="w-[24px] h-[24px] bgactive rounded-full"></div>
+                    </div>
+                    <div className="absolute top-[25px] left-[28px] w-full h-[1px] bg-gradient-to-r from-[#7E5AA3] to-[#23ABD2]"></div>
+                  </div> 
 
-              {/* Image */}
-              <div className="absolute -top-[26%] left-[35%] z-[2]">
-                <Image
-                  src={item.image}
-                  alt={item.title}
-                  width={500}
-                  height={500}
-                  className="object-cover rounded-[12px] h-[428px]"
+                  {/* Animated Content */}
+                  <motion.div
+                    initial={{ opacity: 0, y: 50 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    viewport={{ once: false, amount: 0.3 }}
+                    transition={{ duration: 0.6, ease: "easeOut" }}
+                    className="md:mt-5 lg:mt-[71px] vhidden"
+                  >
+                    <p className="text-[1.3rem] md:text-md xl:text-lg 2xl:text-xl font-light text-black leading-[1.18] mb-3">
+                      {item.title}
+                    </p>
+                    <div
+                      className="text-sm font-light text-colorpara leading-[1.2] max-w-[24ch]"
+                      dangerouslySetInnerHTML={{ __html: item.description }}
+                    ></div>
+                  </motion.div>
+
+                  {/* Animated Image */}
+                  <motion.div
+                    initial={{ opacity: 0, scale: 0.9, y: -30 }}
+                    whileInView={{ opacity: 1, scale: 1, y: -80 }}
+                    viewport={{ once: false, amount: 0.3 }}
+                    transition={{ duration: 0.9, ease: "easeOut", delay: 0.1 }}
+                    className="md:absolute  md:left-[40%] lg:left-[35%] z-[2] imgtrans"
+                  >
+                    <Image
+                      src={item.image}
+                      alt={item.title}
+                      width={500}
+                      height={500}
+                      className="object-cover rounded-[12px] h-[300px] w-full lg:h-[380px] xl:h-[428px] md:w-[85%] lg:w-[85%] 2xl:w-[100%]"
+                    />
+                  </motion.div>
+                </motion.div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+
+          {/* Pagination + Navigation */} 
+         <div className="flex gap-9 w-fit m-auto relative top-5 lg:top-10 left-0 xl:left-12">
+            <button
+              className="swiper-button-prev-custom transition cursor-pointer group"
+              aria-label="Previous"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                className="group-hover:scale-140  duration-300"
+              >
+                <path
+                  d="M15.0901 19.9201L8.57009 13.4001C7.80009 12.6301 7.80009 11.3701 8.57009 10.6001L15.0901 4.08008"
+                  stroke="#626262"
+                  strokeWidth="1.5"
+                  strokeMiterlimit="10"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 />
-              </div>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-      
-      <div className="commentpagination cstss rounded-2xl swiper-pagination-custom absolute !-bottom-12 !left-[50%] !translate-x-[-50%]  !w-fit !m-auto bg-[#D3D3D3] flex justify-center gap-0"></div>
- 
-    </div>
+              </svg>
+            </button>
+
+            <div className="commentpagination cstss rounded-2xl swiper-pagination-custom !w-fit !m-auto bg-[#D3D3D3] flex justify-center gap-0"></div>
+
+            <button
+              className="swiper-button-next-custom transition cursor-pointer group"
+              aria-label="Next"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                className="group-hover:scale-140  duration-300 rotate-180"
+              >
+                <path
+                  d="M15.0901 19.9201L8.57009 13.4001C7.80009 12.6301 7.80009 11.3701 8.57009 10.6001L15.0901 4.08008"
+                  stroke="#626262"
+                  strokeWidth="1.5"
+                  strokeMiterlimit="10"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </button>
+          </div> 
         </div>
       </div>
     </section>
