c4bb2b481d50be1f87e010fb9877af3bb56f141c
Hisham I Globalsurf <hisham@globalsurf.ae>
2025-10-14 12:33:12 +0530

    fix(Press Release: ui)

--- app/components/PressRelease/sections/PopularNews.tsx
+++ app/components/PressRelease/sections/PopularNews.tsx
@@ -4,8 +4,6 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import { moveUp } from "../../motionVarients";
 
-
-
 export interface RecentNewsData {
   title: string;
   category: string[];
@@ -24,19 +22,28 @@ export interface RecentNewsData {
   }[];
 }
 
-const PopularNews = ({
-  data,
-}: {
-  data: RecentNewsData;
-}) => {
+const PopularNews = ({ data }: { data: RecentNewsData }) => {
   return (
     <div className="finsd">
-      {data.popularnews.map((item, index) => (
-        <motion.div variants={moveUp(index * 0.2)} initial="hidden" whileInView="show" viewport={{amount: 0.1, once: true}} key={index} className="chklast">
+      {data.popularnews.slice(0, 3).map((item, index) => (
+        <motion.div
+          variants={moveUp(index * 0.2)}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ amount: 0.1, once: true }}
+          key={index}
+          className="chklast"
+        >
           <div className="flex gap-5">
             <div className="w-[130px] md:w-[171px]">
               <div className="relative h-full">
-                <Image src={item.image} alt="" width={486} height={301} className="rounded-lg img-fluid object-cover w-full h-full max-h-[171px]" />
+                <Image
+                  src={item.image}
+                  alt=""
+                  width={486}
+                  height={301}
+                  className="rounded-lg img-fluid object-cover w-full h-full max-h-[171px]"
+                />
                 <div className="absolute inset-0 rounded-lg bg-[linear-gradient(180.12deg,rgba(0,0,0,0)_50%,rgba(0,0,0,0.75)_99.9%)] "></div>
               </div>
             </div>
@@ -46,15 +53,26 @@ const PopularNews = ({
                 <p>{item.category}</p>
               </div>
               <div className="mt-4">
-                <p className="max-w-[11ch] text-[1.3rem] md:text-md xl:text-lg 2xl:text-xl font-light text-black leading-[1.18] lettersp-1 overflow-hidden text-ellipsis line-clamp-2 display[-webkit-box] [-webkit-line-clamp:2] [-webkit-box-orient:vertical]">{item.description}</p>
+                <p className="max-w-[11ch] text-[1.3rem] md:text-md xl:text-lg 2xl:text-xl font-light text-black leading-[1.18] lettersp-1 overflow-hidden text-ellipsis line-clamp-2 display[-webkit-box] [-webkit-line-clamp:2] [-webkit-box-orient:vertical]">
+                  {item.description}
+                </p>
               </div>
-              <div className="mt-4 p-1 flex items-center justify-center bg-primary w-[27px] h-[27px] rounded-full transition-transform
-                                 duration-300  rotate-45">
-                <Image src="/assets/arrow.svg" alt="arrow" width={11} height={11} />
+              <div
+                className="mt-4 p-1 flex items-center justify-center bg-primary w-[27px] h-[27px] rounded-full transition-transform
+                                 duration-300  rotate-45"
+              >
+                <Image
+                  src="/assets/arrow.svg"
+                  alt="arrow"
+                  width={11}
+                  height={11}
+                />
               </div>
             </div>
           </div>
-          <div className="py-3 lg:py-7 linediv"><div className="bg-[#D3D3D3] h-[1px] w-full"></div></div>
+          <div className="py-3 lg:py-7 linediv">
+            <div className="bg-[#D3D3D3] h-[1px] w-full"></div>
+          </div>
         </motion.div>
       ))}
     </div>
--- app/components/PressRelease/sections/UpcomingEvents.tsx
+++ app/components/PressRelease/sections/UpcomingEvents.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 import { moveUp } from "../../motionVarients";
 
-
 export interface RecentNewsData {
   title: string;
   category: string[];
@@ -24,27 +23,30 @@ export interface RecentNewsData {
   upcomingData: {
     image: string;
     date: string;
-    day: number,
+    day: number;
     category: string;
     description: string;
   }[];
-
 }
 
-const UpcomingEvents = ({
-  data,
-}: {
-  data: RecentNewsData;
-}) => {
+const UpcomingEvents = ({ data }: { data: RecentNewsData }) => {
   return (
     <div className="finsd">
-      {data.upcomingData.map((item, index) => (
-        <motion.div variants={moveUp(index * 0.2)} initial="hidden" whileInView="show" viewport={{amount: 0.1, once: true}} key={index} className="chklast">
+      {data.upcomingData.slice(0, 3).map((item, index) => (
+        <motion.div
+          variants={moveUp(index * 0.2)}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ amount: 0.1, once: true }}
+          key={index}
+          className="chklast"
+        >
           <div className="flex gap-5">
             <div className="w-[100px] md:w-[171px]">
               <div className="relative h-full bg-[#C9F3FF] rounded-xl">
                 <div className="bg-[#42BADC] rounded-t-xl text-sm py-2 md:py-4 font-light  text-white text-center">
-                  <p>{item.date}</p></div>
+                  <p>{item.date}</p>
+                </div>
                 <div className="flex items-center justify-center">
                   <p className="text-xl text-black pt-4 lg:pt-5">{item.day}</p>
                 </div>
@@ -55,15 +57,26 @@ const UpcomingEvents = ({
                 <p>{item.category}</p>
               </div>
               <div className="mt-2 md:mt-4">
-                <p className="max-w-[11ch] text-[1.3rem] md:text-md xl:text-lg 2xl:text-xl font-light text-black leading-[1.18] lettersp-1 overflow-hidden text-ellipsis line-clamp-2 display[-webkit-box] [-webkit-line-clamp:2] [-webkit-box-orient:vertical]">{item.description}</p>
+                <p className="max-w-[11ch] text-[1.3rem] md:text-md xl:text-lg 2xl:text-xl font-light text-black leading-[1.18] lettersp-1 overflow-hidden text-ellipsis line-clamp-2 display[-webkit-box] [-webkit-line-clamp:2] [-webkit-box-orient:vertical]">
+                  {item.description}
+                </p>
               </div>
-              <div className="mt-2 md:mt-4 p-1 flex items-center justify-center bg-primary w-[27px] h-[27px] rounded-full transition-transform
-                                 duration-300  rotate-45">
-                <Image src="/assets/arrow.svg" alt="arrow" width={11} height={11} />
+              <div
+                className="mt-2 md:mt-4 p-1 flex items-center justify-center bg-primary w-[27px] h-[27px] rounded-full transition-transform
+                                 duration-300  rotate-45"
+              >
+                <Image
+                  src="/assets/arrow.svg"
+                  alt="arrow"
+                  width={11}
+                  height={11}
+                />
               </div>
             </div>
           </div>
-          <div className="py-3 lg:py-7 linediv"><div className="bg-[#D3D3D3] h-[1px] w-full"></div></div>
+          <div className="py-3 lg:py-7 linediv">
+            <div className="bg-[#D3D3D3] h-[1px] w-full"></div>
+          </div>
         </motion.div>
       ))}
     </div>
