f6a6e839166c530eec65974d2c54f2db415b9ec2
Nived <nivuyt789@gmail.com>
2025-10-30 15:42:39 +0530

    feat:api connections

--- app/(user)/beam-schools/school-scholarship-programs/page.tsx
+++ app/(user)/beam-schools/school-scholarship-programs/page.tsx
@@ -1,9 +1,14 @@
 import Index from "@/app/components/Scholarship";
 
-const Page = () => {
+const Page = async () => {
+  const response = await fetch(`${process.env.BASE_URL}/api/admin/scholarship-programs`, {
+    next: { revalidate: 60 },
+  });
+  const data = await response.json();
+
   return (
     <>
-      <Index />
+      <Index data={data.data} />
     </>
   );
 };
--- app/components/AccrAffil/sections/Main.tsx
+++ app/components/AccrAffil/sections/Main.tsx
@@ -3,18 +3,20 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 import { moveRight, moveUp } from "../../motionVarients";
 import SplitText from "@/components/SplitText";
-const Main = () => {
+import { AccreditationProps } from "../type";
+
+const Main = ({data}: {data: AccreditationProps['firstSection']}) => {
   return ( 
     <section className="pt-10 xl:pt-25 2xl:pt-[135px] pb-6 xl:pb-12 2xl:pb-[50px]">
       <div className="container">
         <motion.div variants={moveUp(0.2)} initial="hidden" whileInView="show" viewport={{ amount: 0.1, once: false, margin: `-250px 0px -250px 0px`, }} className="relative overflow-hidden rounded-xl p-5 md:p-10 xl:p-15 2xl:min-h-[635px] flex items-end">
           <div className="absolute top-0 left-0 w-full h-full bg-black/40 z-10"></div>
           <motion.div variants={moveRight(0.2)} initial="hidden" whileInView="show" viewport={{ amount: 0.1, once: false, margin: `-250px 0px -250px 0px`, }} className="absolute top-0 left-0 w-[90%] h-full bg-gradient-to-r from-[#066B7F] to-black/0 z-10 opacity-95"></motion.div>
-          <Image src="/assets/images/accr-affil/main.jpg" alt="" width={1920} height={1280} className="absolute top-0 left-0 z-0 h-full object-cover" />
+          <Image src={data.image} alt="" width={1920} height={1280} className="absolute top-0 left-0 z-0 h-full object-cover" />
           <div className="relative z-30 text-white">
             <SplitText
               tag="h2"
-              text="Accrediation"
+              text={data.title}
               className="text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl leading-[1.111111111111111] mb-5 font-light"
               delay={100}
               duration={0.6}
@@ -28,7 +30,7 @@ const Main = () => {
             />
             <SplitText
               tag="p"
-              text="Our schools continually pursue international recognition and seek accreditation to reinforce a commitment to continuous improvement, innovation, and the security of our students and parents. The recognition of New England Association for Schools and Colleges (NEASC) and British Schools Overseas (BSO) is a confirmation of high-quality learning in all specific divisions. Having accredited status for our schools provides recognition of the ability to serve the wider community and the world with a focus on quality education."
+              text={data.description}
               className="font-light max-w-[98ch] text-sm"
               delay={100}
               duration={0.6}
