bdf6ec992e0e4ef75da5c3b661927c5bb062a63a
Hisham I Globalsurf <hisham@globalsurf.ae>
2025-10-31 12:40:57 +0530

    fix(blog-details: connected breadcrump, share btn, implemented reading time functionality)

--- app/(user)/news-&-media/blog/blog-details/[slug]/page.tsx
+++ app/(user)/news-&-media/blog/blog-details/[slug]/page.tsx
@@ -1,8 +1,8 @@
 import Index from "@/app/components/BlogDetails";
 
-const Page = async({params}: {params: Promise<{slug: string}>}) => {
-    const slug = (await params).slug;
-    const response = await fetch(
+const Page = async ({ params }: { params: Promise<{ slug: string }> }) => {
+  const slug = (await params).slug;
+  const response = await fetch(
     `${process.env.BASE_URL}/api/admin/blogs?slug=${slug}`,
     { next: { revalidate: 60 } }
   );
@@ -12,9 +12,9 @@ const Page = async({params}: {params: Promise<{slug: string}>}) => {
     { next: { revalidate: 60 } }
   );
 
-    const allBlogData = await allBlogResponse.json();
-    const data = await response.json();
-    return <Index data={data.data} allBlogData={allBlogData.data}/>;
-}
+  const allBlogData = await allBlogResponse.json();
+  const data = await response.json();
+  return <Index data={data.data} allBlogData={allBlogData.data} />;
+};
 
 export default Page;
