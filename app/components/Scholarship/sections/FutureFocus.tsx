ac264b0673e34cd3fc7e3fcb0f7be077ee222e83
Nived <nived@globalsurf.ae>
2025-12-02 10:03:17 +0530

    refactor:helper function for isArabic check

--- app/components/Scholarship/sections/FutureFocus.tsx
+++ app/components/Scholarship/sections/FutureFocus.tsx
@@ -5,6 +5,8 @@ import { motion } from "framer-motion";
 import SplitText from "@/components/SplitText";
 import { moveUp } from "../../motionVarients";
 import { ScholarshipProps } from "../type";
+import useIsPreferredLanguageArabic from "@/lib/getPreferredLanguage";
+import {applyLang} from "@/lib/applyLang";
 
 
 const FutureFocus = ({
@@ -12,6 +14,8 @@ const FutureFocus = ({
 }: {
   data: ScholarshipProps['firstSection'];
 }) => {
+  const isArabic = useIsPreferredLanguageArabic();
+  const t = applyLang(isArabic, data);
   return (
     <section className="py-8 md:py-12 lg:py-20 2xl:py-[135px]">
       <div className="container">
@@ -20,7 +24,7 @@ const FutureFocus = ({
           <div>
             <SplitText
               tag="h2"
-              text={data.title}
+              text={t.title}
               className=" text-lg xl:text-2xl 2xl:text-4xl 2xl:max-w-[8ch] font-light leading-[1.111111111] text-black mb-4 md:mb-6 xl:mb-8 2xl:mb-12"
               delay={100}
               duration={0.6}
@@ -35,7 +39,7 @@ const FutureFocus = ({
 
             <SplitText
               tag="p"
-              text={data.description}
+              text={t.description}
               className=" text-sm leading-[1.526315789473684]  font-light  text-colorpara"
               delay={100}
               duration={0.6}
@@ -51,7 +55,7 @@ const FutureFocus = ({
           </div>
           {/* Right Image */}
           <motion.div variants={moveUp(1.25)} initial="hidden" whileInView="show" viewport={{ once: true, amount: 0.1 }} className="relative w-full h-[250px] md:h-auto rounded-[12px] overflow-hidden">
-            <Image src={data.image} alt={data.imageAlt} fill className="object-cover transition-all duration-500" />
+            <Image src={data.image} alt={t.imageAlt} fill className="object-cover transition-all duration-500" />
             <motion.div
               className="absolute bottom-0 w-full h-[60%] bg-gradient-to-b from-black/0 to-[#42BADCC9]/79"
               initial={{ y: "100%" }}
