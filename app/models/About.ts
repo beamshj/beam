ea49afc5a264e8fa55df8da1e0ef399302702267
Hisham I Globalsurf <hisham@globalsurf.ae>
2025-12-02 10:06:31 +0530

    feat(setup for arabic language: admin/about)

--- app/models/About.ts
+++ app/models/About.ts
@@ -1,197 +1,312 @@
 import mongoose from "mongoose";
 
 const aboutSchema = new mongoose.Schema({
-    banner: {
-        type: String,
-        required: true
+  banner: {
+    type: String,
+    required: true,
+  },
+  bannerAlt: {
+    type: String,
+  },
+  bannerAlt_ar: {
+    type: String,
+  },
+  metaTitle: {
+    type: String,
+    required: true,
+  },
+  metaTitle_ar: {
+    type: String,
+  },
+  metaDescription: {
+    type: String,
+    required: true,
+  },
+  metaDescription_ar: {
+    type: String,
+  },
+  pageTitle: {
+    type: String,
+    required: true,
+  },
+  pageTitle_ar: {
+    type: String,
+  },
+  firstSection: {
+    mainTitle: {
+      type: String,
+      required: true,
     },
-    bannerAlt: {
-        type: String,
+    mainTitle_ar: {
+      type: String,
     },
-    metaTitle: {
-        type: String,
-        required: true
+    subTitle: {
+      type: String,
+      required: true,
     },
-    metaDescription: {
-        type: String,
-        required: true
+    subTitle_ar: {
+      type: String,
     },
-    pageTitle: {
-        type: String,
-        required: true
+    highlight: {
+      type: String,
+      required: true,
     },
-    firstSection: {
-        mainTitle: {
-            type: String,
-            required: true
+    highlight_ar: {
+      type: String,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    description_ar: {
+      type: String,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    imageAlt: {
+      type: String,
+    },
+    imageAlt_ar: {
+      type: String,
+    },
+  },
+  secondSection: {
+    title: {
+      type: String,
+      required: true,
+    },
+    title_ar: {
+      type: String,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    description_ar: {
+      type: String,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    imageAlt: {
+      type: String,
+    },
+    imageAlt_ar: {
+      type: String,
+    },
+    items: [
+      {
+        logo: {
+          type: String,
+          required: true,
         },
-        subTitle: {
-            type: String,
-            required: true
+        logoAlt: {
+          type: String,
         },
-        highlight: {
-            type: String,
-            required: true
+        logoAlt_ar: {
+          type: String,
+        },
+        title: {
+          type: String,
+          required: true,
+        },
+        title_ar: {
+          type: String,
         },
         description: {
-            type: String,
-            required: true
+          type: String,
+          required: true,
         },
+        description_ar: {
+          type: String,
+        },
+      },
+    ],
+  },
+  thirdSection: {
+    title: {
+      type: String,
+      required: true,
+    },
+    title_ar: {
+      type: String,
+    },
+    items: [
+      {
         image: {
-            type: String,
-            required: true
+          type: String,
+          required: true,
         },
         imageAlt: {
-            type: String,
+          type: String,
         },
-    },
-    secondSection: {
+        imageAlt_ar: {
+          type: String,
+        },
         title: {
-            type: String,
-            required: true
+          type: String,
+          required: true,
+        },
+        title_ar: {
+          type: String,
         },
         description: {
-            type: String,
-            required: true
+          type: String,
+          required: true,
+        },
+        description_ar: {
+          type: String,
+        },
+      },
+    ],
+  },
+  historySection: {
+    title: {
+      type: String,
+      required: true,
+    },
+    title_ar: {
+      type: String,
+    },
+    items: [
+      {
+        year: {
+          type: String,
+          required: true,
+        },
+        year_ar: {
+          type: String,
+        },
+        title: {
+          type: String,
+          required: true,
+        },
+        title_ar: {
+          type: String,
+        },
+        description: {
+          type: String,
+          required: true,
+        },
+        description_ar: {
+          type: String,
         },
         image: {
-            type: String,
-            required: true
+          type: String,
+          required: true,
         },
         imageAlt: {
-            type: String,
+          type: String,
         },
-        items: [{
-            logo: {
-                type: String,
-                required: true
-            },
-            logoAlt: {
-                type: String,
-            },
-            title: {
-                type: String,
-                required: true
-            },
-            description: {
-                type: String,
-                required: true
-            }
-        }]
+        imageAlt_ar: {
+          type: String,
+        },
+      },
+    ],
+  },
+  fifthSection: {
+    title: {
+      type: String,
+      required: true,
     },
-    thirdSection: {
-        title: {
-            type: String,
-            required: true
-        },
-        items: [{
-            image: {
-                type: String,
-                required: true
-            },
-            imageAlt: {
-                type: String,
-            },
-            title: {
-                type: String,
-                required: true
-            },
-            description: {
-                type: String,
-                required: true
-            }
-        }]
+    title_ar: {
+      type: String,
     },
-    historySection: {
-        title: {
-            type: String,
-            required: true
-        },
-        items: [{
-            year: {
-                type: String,
-                required: true
-            },
-            title: {
-                type: String,
-                required: true
-            },
-            description: {
-                type: String,
-                required: true
-            },
-            image: {
-                type: String,
-                required: true
-            },
-            imageAlt: {
-                type: String,
-            }
-        }]
+    description: {
+      type: String,
+      required: true,
     },
-    fifthSection: {
-        title: {
-            type: String,
-            required: true
-        },
-        description: {
-            type: String,
-            required: true
-        },
+    description_ar: {
+      type: String,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    imageAlt: {
+      type: String,
+    },
+    imageAlt_ar: {
+      type: String,
+    },
+  },
+  sixthSection: {
+    title: {
+      type: String,
+      required: true,
+    },
+    title_ar: {
+      type: String,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    description_ar: {
+      type: String,
+    },
+    items: [
+      {
         image: {
-            type: String,
-            required: true
+          type: String,
+          required: true,
         },
         imageAlt: {
-            type: String,
+          type: String,
         },
-    },
-    sixthSection: {
+        imageAlt_ar: {
+          type: String,
+        },
         title: {
-            type: String,
-            required: true
+          type: String,
+          required: true,
         },
-        description: {
-            type: String,
-            required: true
+        title_ar: {
+          type: String,
         },
-        items: [{
-            image: {
-                type: String,
-                required: true
-            },
-            imageAlt: {
-                type: String,
-            },
-            title: {
-                type: String,
-                required: true
-            }
-        }]
+      },
+    ],
+  },
+  seventhSection: {
+    title: {
+      type: String,
+      required: true,
     },
-    seventhSection: {
-        title: {
-            type: String,
-            required: true
+    title_ar: {
+      type: String,
+    },
+    items: [
+      {
+        image: {
+          type: String,
+          required: true,
         },
-        items: [{
-            image: {
-                type: String,
-                required: true
-            },
-            imageAlt: {
-                type: String,
-            },
-            title: {
-                type: String,
-                required: true
-            },
-            link: {
-                type: String,
-                required: true
-            }
-        }]
-    }
-})
+        imageAlt: {
+          type: String,
+        },
+        imageAlt_ar: {
+          type: String,
+        },
+        title: {
+          type: String,
+          required: true,
+        },
+        title_ar: {
+          type: String,
+        },
+        link: {
+          type: String,
+          required: true,
+        },
+        link_ar: {
+          type: String,
+        },
+      },
+    ],
+  },
+});
 
 export default mongoose.models.About || mongoose.model("About", aboutSchema);
